d61bc1f47610033fba33eab8277e5df7a2647e0b
Fernando Fernández <fernfdez@gmail.com>
2013-04-01 10:21:15 +0200

    RC - Text-properties plugin w/o translation
    - Size, color, stroke & shadow for text

--- static/js/textProperties.js
+++ static/js/textProperties.js
@@ -5,6 +5,10 @@ function TextProperties (options) {
     this.fillColor = options['fillColor'] || '#e7a61a';
     this.strokeWidth = options['strokeWidth'] || 0;
     this.fontSize = options['fontSize'] || 20;
+    this.vShadow = options['vShadow'] || 0;
+    this.hShadow = options['hShadow'] || 0;
+    this.bShadow = options['bShadow'] || 0;
+    this.cShadow = options['cShadow'] || '#000000';
 };
 
 TextProperties.prototype.refresh = function () {
@@ -12,17 +16,53 @@ TextProperties.prototype.refresh = function () {
     strokeColorText = this.strokeColor;
     fillColorText = this.fillColor;
     fontSizeText = this.fontSize + 'px';
+    ffstrokeText = '';
+    shadowText = '';
+    shadow = false;
+    i=0;
+    if (this.vShadow != 0 || this.hShadow !=0 || this.bShadow !=0){
+        shadowText = this.hShadow+'px '+this.vShadow+'px '+this.bShadow+'px '+this.cShadow;
+        shadow = true;
+    } else {
+        shadow = false;
+    }
+    while (i<this.strokeWidth){
+        i++;
+        if (i == this.strokeWidth){
+            ffstrokeText += i+'px '+i+'px '+this.strokeColor+', -'+i+'px -'+i+'px '+this.strokeColor+', -'+i+'px '+i+'px '+this.strokeColor+', '+i+'px -'+i+'px '+this.strokeColor;
+        } else {
+            ffstrokeText += i+'px '+i+'px '+this.strokeColor+', -'+i+'px -'+i+'px '+this.strokeColor+', -'+i+'px '+i+'px '+this.strokeColor+', '+i+'px -'+i+'px '+this.strokeColor+', ';           
+        }
+    }
 
     this.htmlElement.css('font-size', this.fontSize);
+    this.htmlElement.css('color', this.fillColor);
     this.htmlElement.css('-webkit-text-stroke-width', this.strokeWidth);
     this.htmlElement.css('-webkit-text-stroke-color', this.strokeColor);
     this.htmlElement.css('-webkit-text-fill-color', this.fillColor);
 
     this.htmlCode.html('');
     var text = '<div>font-size: ' + fontSizeText +';</div>';
-    text += '<div>-webkit-text-stroke-width: ' + strokeWidthText +';</div>';
-    text += '<div>-webkit-text-stroke-color: ' + strokeColorText +';</div>';
-    text += '<div>-webkit-text-fill-color: ' + fillColorText +';</div>';
+    text += '<div>color: ' + fillColorText +';</div>';
+    if (this.strokeWidth != 0 || shadow == true){
+        if (this.strokeWidth != 0 && shadow == true){
+            text += '<div>text-shadow: ' +ffstrokeText+', '+shadowText +';</div>';
+            text += '<div>-webkit-text-stroke-width: ' + strokeWidthText +';</div>';
+            text += '<div>-webkit-text-stroke-color: ' + strokeColorText +';</div>';
+            text += '<div>-webkit-text-fill-color: ' + fillColorText +';</div>';
+            this.htmlElement.css('text-shadow', ffstrokeText+', '+shadowText);
+        } else if (this.strokeWidth == 0 && shadow == true){
+            text += '<div>text-shadow: ' +shadowText +';</div>';
+            this.htmlElement.css('text-shadow', shadowText);
+        } else {
+            this.htmlElement.css('text-shadow', ffstrokeText);
+            text += '<div>text-shadow: ' +ffstrokeText+';</div>';
+            text += '<div>-webkit-text-stroke-width: ' + strokeWidthText +';</div>';
+            text += '<div>-webkit-text-stroke-color: ' + strokeColorText +';</div>';
+            text += '<div>-webkit-text-fill-color: ' + fillColorText +';</div>';
+        }
+        
+    }    
     this.htmlCode.append(text);
 };
 
@@ -30,17 +70,20 @@ TextProperties.prototype.fontSize = function (fontSize) {
     this.fontSize = fontSize;
 };
 
-TextProperties.prototype.strokeWidth = function (border) {
-    this.strokeWidth = border;
+TextProperties.prototype.strokeWidth = function (strokeWidth) {
+    this.strokeWidth = strokeWidth;
 };
 
 function _getAllValuesFromPanelBorderRadius() {
     var options = {};
-    options['borderStyle'] = $('#select-border :selected').val();
     options['strokeColor'] = $('#tp-color').val();
     options['fillColor'] = $('#tp-background-color').val();
     options['strokeWidth'] = parseFloat($('#stroke-width').val());
     options['fontSize'] = parseFloat($('#font-size').val());
+    options['hShadow'] = parseFloat($('#horizontal-shadow').val());
+    options['vShadow'] = parseFloat($('#vertical-shadow').val());
+    options['bShadow'] = parseFloat($('#blur-shadow').val());
+    options['cShadow'] = $('#tp-shadow-color').val();
     return options;
 };
 
@@ -65,7 +108,7 @@ $('body').ready(function() {
     copy_text_button($('#copy-text-input'), $('#text-properties-code'));
 
     /* Text Edit */
-    $('#text-value').on(function(){
+    $('#text-value').keyup(function(){
         text = $(this).val();
         $('#textExample').html(text);
     });
@@ -75,6 +118,14 @@ $('body').ready(function() {
         textProperties.refresh();
 
         $('#slider-font-size').slider('value', val);
+    });
+
+    $('#stroke-width').live('keyup', function() {
+        var val = _getFromField($(this).val(), 0, 10, $('#stroke-width'));
+        textProperties.strokeWidth = val;
+        textProperties.refresh();
+
+        $('#slider-stroke-width').slider('value', val);
     });
 
     $('#slider-font-size').slider({
@@ -106,6 +157,68 @@ $('body').ready(function() {
         }
     });
 
+    /* Text shadow */
+    $('#vertical-shadow').live('keyup', function() {
+        var val = _getFromField($(this).val(), -100, 100, $('#vertical-shadow'));
+        textProperties.vShadow = val;
+        textProperties.refresh();
+
+        $('#slider-vertical-shadow').slider('value', val);
+    });
+    $('#horizontal-shadow').live('keyup', function() {
+        var val = _getFromField($(this).val(), -100, 100, $('#horizontal-shadow'));
+        textProperties.hShadow = val;
+        textProperties.refresh();
+
+        $('#slider-horizontal-shadow').slider('value', val);
+    });
+    $('#blur-shadow').live('keyup', function() {
+        var val = _getFromField($(this).val(), 0, 50, $('#blur-shadow'));
+        textProperties.bShadow = val;
+        textProperties.refresh();
+
+        $('#slider-blur-shadow').slider('value', val);
+    });
+    $('#slider-vertical-shadow').slider({
+        value: 0,
+        min: -100,
+        max: 100,
+        step: 1,
+        slide: function(event, ui) {
+            var val = _getFromField(ui.value, -100, 100);
+            textProperties.vShadow = val;
+            textProperties.refresh();
+
+            $('#vertical-shadow').val(val);
+        }
+    });
+    $('#slider-horizontal-shadow').slider({
+        value: 0,
+        min: -100,
+        max: 100,
+        step: 1,
+        slide: function(event, ui) {
+            var val = _getFromField(ui.value, -100, 100);
+            textProperties.hShadow = val;
+            textProperties.refresh();
+
+            $('#horizontal-shadow').val(val);
+        }
+    });
+    $('#slider-blur-shadow').slider({
+        value: 0,
+        min: 0,
+        max: 50,
+        step: 1,
+        slide: function(event, ui) {
+            var val = _getFromField(ui.value, 0, 50);
+            textProperties.bShadow = val;
+            textProperties.refresh();
+
+            $('#blur-shadow').val(val);
+        }
+    });
+
     /* Color (Border and background) */
     $('#tp-color').live('change', function () {
         textProperties.strokeColor = $(this).val();
@@ -120,6 +233,16 @@ $('body').ready(function() {
     });
 
     $('#tp-background-color-button').live('click', function () {
+        $(this).ColorPickerShow();
+    });
+
+    $('#tp-shadow-color').live('change', function () {
+        textProperties.cShadow = $(this).val();
+        textProperties.refresh();
+        $('#tp-shadow-color-button').css('background-color', '#' + $(this).val());
+    });
+
+    $('#tp-shadow-color-button').live('click', function () {
         $(this).ColorPickerShow();
     });
 
@@ -165,6 +288,28 @@ $('body').ready(function() {
         },
         onBeforeShow: function () {
             $(this).ColorPickerSetColor($('#tp-color').val());
+        },
+    })
+    .bind('keyup', function(){
+        $(this).ColorPickerSetColor(this.value);
+    });
+
+    $('#tp-shadow-color-button').ColorPicker({
+        onChange: function(hsb, hex, rgb, el) {
+            textProperties.cShadow = '#' + hex;
+            textProperties.refresh();
+            $('#tp-shadow-color').val('#' + hex);
+            $('#tp-shadow-color-button').css('background-color', '#' + hex);
+        },
+        onSubmit: function(hsb, hex, rgb, el) {
+            textProperties.cShadow = '#' + hex;
+            textProperties.refresh();
+            $('#tp-shadow-color').val('#' + hex);
+            $('#tp-shadow-color-button').css('background-color', '#' + hex);
+            $(el).ColorPickerHide();
+        },
+        onBeforeShow: function () {
+            $(this).ColorPickerSetColor($('#tp-shadow-color').val());
         },
     })
     .bind('keyup', function(){
